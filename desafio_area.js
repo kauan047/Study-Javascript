065d6cdd0b9b91f14ed026958b282126a12b4f79
Kauan <kauanjr875@gmail.com>
2024-08-15 00:11:53 -0300

    Adicionando alguns assuntos aprendidos

---
+++ desafio_area.js
@@ -0,0 +1,9 @@
+// PI * raio **2
+
+
+let raio = 10;
+const pi = 3.14;
+let area = pi*(raio**2);
+
+console.log("A area eh igual a:"area);
+console.log(Math.PI;)
---
+++ templateString.js
@@ -0,0 +1,12 @@
+const nome = 'Rebeca'
+const concatecao = 'Olá' + nome + '!'
+const template = `
+Olá
+
+${nome}!`
+console.log(Concatenacao, template)
+
+console.log('1 + 1 = ${1 + 1}`)
+
+    const up = texto => texto.toUpperCase()
+    console.log(`Ei... ${up('cuidado')}`)
