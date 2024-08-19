4cf0fdbaf6b1b806daa135d0334f43d3e29214e5
kauan047 <kauanjr875@gmail.com>
2024-08-19 11:50:22 -0300

    Adicinando novos arquivos do estudo fundamentos de javascript

---
+++ cubonum.js
@@ -0,0 +1,4 @@
+
+const prompt = require('prompt-sync')();
+
+var primeiro = prompt('Insira um numero');
---
+++ fcbomdiamundo.js
@@ -0,0 +1,10 @@
+
+
+function BomDia(){
+
+ 
+  console.log('Bom dia, Mundo');
+
+}
+
+BomDia()
